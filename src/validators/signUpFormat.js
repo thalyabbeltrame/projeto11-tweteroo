c48593e8e6748297cd266f67ca5bd934d6c73d67
Thalya Busato Beltrame <beltrame.thalya@gmail.com>
2022-06-20 15:14:44 -0300

    final version

--- src/validators/signUpFormat.js
+++ src/validators/signUpFormat.js
@@ -1,9 +1,9 @@
-function validateSignUpFormat(user) {
+const validateSignUpFormat = (user) => {
   const isObject = typeof user === 'object';
   const hasTwoProperties = Object.entries(user).length === 2;
   const isUsernameFormatValid = typeof user.username === 'string';
   const isAvatarFormatValid = typeof user.avatar === 'string';
   return isObject && hasTwoProperties && isUsernameFormatValid && isAvatarFormatValid;
-}
+};
 
 export default validateSignUpFormat;
